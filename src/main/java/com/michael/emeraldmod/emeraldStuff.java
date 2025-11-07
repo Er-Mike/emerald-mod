0df08368856f7a3dd3d7801a76ad702cc5ad1dd0
Er-Mike <enrico.esterasi@outlook.it>
2025-11-07 15:49:38 +0100

    sistemate opzioni della mod

---
+++ src/main/java/com/michael/emeraldmod/emeraldStuff.java
@@ -0,0 +1,13 @@
+package com.michael.emeraldmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(EmeraldMod.MOD_ID)
+public class EmeraldMod {
+    public static final String MOD_ID = "emeraldmod";
+
+    public EmeraldMod() {
+        // Qui in futuro registrerai gli oggetti
+        System.out.println("Emerald Mod caricata!");
+    }
+}
